28c46a5abcbed7efb62060f04fdac398954258bf
凤之兮原 <1577310596@qq.com>
2023-02-12 19:52:55 +0800

    feat: finish jsx

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,65 +1,59 @@
-import { React_ELEMENT_TYPE } from '../../shared/ReactSymbols'
+import { React_ELEMENT_TYPE } from '../../shared/ReactSymbols';
 // ReactElement
-import {
-  Type,
-  Key,
-  Ref,
-  Props,
-  ReactElement,
-} from '../../shared/ReactTypes'
+import { Type, Key, Ref, Props, ReactElement } from '../../shared/ReactTypes';
 
 const ReactElement = function (
-  type: Type,
-  key: Key,
-  ref: Ref,
-  props: Props
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
 ): ReactElement {
-  const element = {
-    $$typeof: React_ELEMENT_TYPE,
-    type,
-    key,
-    ref,
-    props,
-    __mark: 'KongmingLatern',
-  }
-  return element
-}
+	const element = {
+		$$typeof: React_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'KongmingLatern'
+	};
+	return element;
+};
 
 export const jsx = (type, config, ...maybeChildren) => {
-  let key: Key = null
-  let ref: Ref = null
-  const props: Props = {}
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
 
-  for (const prop in config) {
-    const val = config[prop]
-    if (prop === 'key') {
-      if (val !== undefined) {
-        key = '' + val
-      }
-      continue
-    }
-    if (prop === 'ref') {
-      if (val !== undefined) {
-        ref = '' + val
-      }
-      continue
-    }
-    if ({}.hasOwnProperty.call(config, prop)) {
-      props[prop] = val
-    }
-  }
-  const maybeChildrenLength = maybeChildren.length
-  if (maybeChildrenLength) {
-    if (maybeChildrenLength === 1) {
-      // child
-      props.children = maybeChildren[0]
-    } else {
-      // [child, child, child, ...]
-      props.children = maybeChildren
-    }
-  }
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = '' + val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			// child
+			props.children = maybeChildren[0];
+		} else {
+			// [child, child, child, ...]
+			props.children = maybeChildren;
+		}
+	}
 
-  return ReactElement(type, key, ref, props)
-}
+	return ReactElement(type, key, ref, props);
+};
 
-export const jsxDev = jsx
+export const jsxDEV = jsx;
