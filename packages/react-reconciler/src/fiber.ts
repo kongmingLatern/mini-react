7a3af439be618e10cb3f818b98c4095afc7fad41
凤之兮原 <1577310596@qq.com>
2023-02-13 10:43:44 +0800

    feat: add FiberNode class

---
+++ packages/react-reconciler/src/fiber.ts
@@ -0,0 +1,39 @@
+import { Props, Key, Ref } from 'shared/ReactTypes'
+import { WorkTag } from './workTag'
+export class FiberNode {
+  tag: WorkTag
+  key: Key
+  stateNode: any
+  pendingProps: Props
+  memoizeProps: Props
+  type: any
+  return: FiberNode | null
+  sibling: FiberNode | null
+  child: FiberNode | null
+  index: number
+  ref: Ref
+  constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+    this.tag = tag
+    this.key = key
+    // 保存 Dom
+    this.stateNode = null
+    // FunctionComponent
+    this.type = null
+
+    /** 构成树状结构 **/
+
+    // 指向父 fiberNode
+    this.return = null
+    // 指向兄弟 fiberNode
+    this.sibling = null
+    this.child = null
+    this.index = 0
+
+    /** 作为工作单元 **/
+    this.pendingProps = pendingProps
+    // 工作之后的 props
+    this.memoizeProps = null
+
+    this.ref = null
+  }
+}
