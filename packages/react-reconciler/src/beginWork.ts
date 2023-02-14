e7eeff9956e79180a2cf047aca338ace46a47059
凤之兮原 <1577310596@qq.com>
2023-02-14 11:18:17 +0800

    feat: finish beginWork

--- packages/react-reconciler/src/beginWork.ts
+++ packages/react-reconciler/src/beginWork.ts
@@ -1,85 +1,62 @@
-import {
-  mountChildFibers,
-  reconcileChildFibers,
-} from './childFibers'
-import {
-  UpdateQueue,
-  processUpdateQueue,
-} from './updateQueue'
-import {
-  HostComponent,
-  HostRoot,
-  HostText,
-} from './workTag'
-import { FiberNode } from './fiber'
-import { ReactElementType } from 'shared/ReactTypes'
+import { mountChildFibers, reconcileChildFibers } from './childFibers';
+import { UpdateQueue, processUpdateQueue } from './updateQueue';
+import { HostComponent, HostRoot, HostText } from './workTag';
+import { FiberNode } from './fiber';
+import { ReactElementType } from 'shared/ReactTypes';
 
-export const beginWork = (
-  wip: FiberNode
-): FiberNode | null => {
-  // 比较，返回子 fiberNode
-  switch (wip.key) {
-    case HostRoot:
-      updateHostRoot(wip)
-      break
+export const beginWork = (wip: FiberNode): FiberNode | null => {
+	// 比较，返回子 fiberNode
+	switch (wip.key) {
+		case HostRoot:
+			updateHostRoot(wip);
+			break;
 
-    case HostComponent:
-      updateHostCompoent(wip)
-      break
-    case HostText:
-      return null
-    default:
-      if (__DEV__) {
-        console.warn('beginWork 发生错误')
-      }
-      break
-  }
-  return null
-}
+		case HostComponent:
+			updateHostCompoent(wip);
+			break;
+		case HostText:
+			return null;
+		default:
+			if (__DEV__) {
+				console.warn('beginWork 发生错误');
+			}
+			break;
+	}
+	return null;
+};
 
 function updateHostCompoent(wip) {
-  // Implement
-  const nextProps = wip.pendingProps
-  const nextChildren = nextProps.children
-  reconcileChildren(wip, nextChildren)
-  return wip.child
+	// Implement
+	const nextProps = wip.pendingProps;
+	const nextChildren = nextProps.children;
+	reconcileChildren(wip, nextChildren);
+	return wip.child;
 }
 
 function updateHostRoot(wip: FiberNode) {
-  // Implement
-  const baseState = wip.menoizeState
-  const updateQueue =
-    wip.updateQueue as UpdateQueue<Element>
-  const pending = updateQueue.shared.pending
-  updateQueue.shared.pending = null
-  const { memoizedState } = processUpdateQueue(
-    baseState,
-    pending
-  )
-  wip.menoizeState = memoizedState
+	// Implement
+	const baseState = wip.menoizeState;
+	const updateQueue = wip.updateQueue as UpdateQueue<Element>;
+	const pending = updateQueue.shared.pending;
+	updateQueue.shared.pending = null;
+	const { memoizedState } = processUpdateQueue(baseState, pending);
+	wip.menoizeState = memoizedState;
 
-  const nextChildren = wip.menoizeState
+	const nextChildren = wip.menoizeState;
 
-  reconcileChildren(wip, nextChildren)
+	reconcileChildren(wip, nextChildren);
 
-  return wip.child
+	return wip.child;
 }
 
-function reconcileChildren(
-  wip: FiberNode,
-  children?: ReactElementType
-) {
-  // Implement
-  const current = wip.alternate
-  if (current) {
-    // update
-    wip.child = reconcileChildFibers(
-      wip,
-      current?.child,
-      children
-    )
-  } else {
-    // mount
-    wip.child = mountChildFibers(wip, null, children)
-  }
+function reconcileChildren(wip: FiberNode, children?: ReactElementType) {
+	// Implement
+	const current = wip.alternate;
+	if (current) {
+		// update
+		wip.child = reconcileChildFibers(wip, current?.child, children);
+	} else {
+		// mount
+		wip.child = mountChildFibers(wip, null, children);
+	}
 }
