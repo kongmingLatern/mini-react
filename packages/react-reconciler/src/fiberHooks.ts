04d359221f35dc1ac65715bf69d74ccdb8345b30
凤之兮原 <1577310596@qq.com>
2023-02-16 11:54:06 +0800

    feat: complete hooks architecture

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,8 +1,36 @@
-import { FiberNode } from './fiber';
-export function renderWithHooks(wip: FiberNode) {
-	const Component = wip.type;
-	const props = wip.pendingProps;
-	const children = Component(props);
+import internals from 'shared/internals'
+import { FiberNode } from './fiber'
 
-	return children;
+let currentlyRenderingFiber: FiberNode | null = null
+let workInProgressHook: Hook | null = null
+
+const { currentDispatcher } = internals
+
+interface Hook {
+  memoizedState: any
+  updateQueue: unknown
+  next: Hook | null
+}
+
+export function renderWithHooks(wip: FiberNode) {
+  // 赋值操作
+  currentlyRenderingFiber = wip
+  wip.memoizedState = null
+
+  const current = wip.alternate
+
+  if (current) {
+    // update
+  } else {
+    // mount
+  }
+
+  const Component = wip.type
+  const props = wip.pendingProps
+  const children = Component(props)
+
+  // 重置操作
+  currentlyRenderingFiber = null
+
+  return children
 }
--- packages/shared/internals.ts
+++ packages/shared/internals.ts
@@ -1,7 +1,6 @@
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from './../react/index'
-import * as React from 'react'
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from './../react/index';
+import * as React from 'react';
 
-const internals =
-  React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+const internals = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
-export default internals
+export default internals;
