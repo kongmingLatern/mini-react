3f5cc50bada0dd886fcbf3c737258a474dcbd58b
凤之兮原 <1577310596@qq.com>
2023-02-16 21:51:12 +0800

    chore: add jest and change some config

--- packages/react-dom/__tests__/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import react from 'react'
-
-describe('123', () => {
-  it('123', () => {
-    console.log('react', react)
-    expect(1).toBe(1)
-  })
-})
--- packages/react-dom/test-utils.ts
+++ packages/react-dom/test-utils.ts
@@ -1,10 +1,8 @@
 // @ts-ignore
-import { createRoot } from 'react-dom'
-import { ReactElementType } from './../shared/ReactTypes'
+import { createRoot } from 'react-dom';
+import { ReactElementType } from './../shared/ReactTypes';
 
-export function renderInfoDocument(
-  element: ReactElementType
-) {
-  const div = document.createElement('div')
-  return createRoot(div).render(element)
+export function renderInfoDocument(element: ReactElementType) {
+	const div = document.createElement('div');
+	return createRoot(div).render(element);
 }
